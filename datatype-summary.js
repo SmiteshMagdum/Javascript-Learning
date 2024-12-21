a09edb2c7b95f37f1349b9403cdb2e3baf4f4cb0
SmiteshMagdum <smiteshmagdum@gmail.com>
2024-12-21 12:41:11 +0530

    stack and heap memory

--- datatype-summary.js
+++ datatype-summary.js
@@ -1,6 +1,6 @@
 //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id===anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//********************************************* */
+// stack (premetive ), heap (non premetive )
+let myYotubeName = "smitesh.com"
+let anotherName = "chaiaurcode"
+
+console.log(myYotubeName);
+console.log(anotherName);
+
+let userone  = {
+    email = "smitesh@gmail.com"
+    upi :"user@bl"
+
+}
+    let userTwo = userone
+
+    userTwo.email ="magdum@gmail.com"
+
+
+    console.log(userone.email);
+    console.log(userTwo.email);
+    
+
+
