026dc94500a2c772df134d681998919e1cad4db8
SmiteshMagdum <smiteshmagdum@gmail.com>
2024-12-22 18:52:49 +0530

    add important line

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -15,12 +15,11 @@ const otherNUmber = 123.6532
 const hundred = 1000000
 //console.log(hundred.toLocaleString('en-IN'));
 //++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++
-
 // console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
-// // console.log(Math.floor(4.9));
+//  console.log(Math.floor(4.9));
 // console.log(Math.sqrt(144))
 // console.log(Math.min(4,6,5,4,9,8,2,1,6,4,0,-9))
 // console.log(Math.max(4,6,5,4,9,8,2,1,6,4,0,-9))
@@ -31,4 +30,5 @@ console.log(Math.floor(Math.random()*10)+1);
 const min = 10
 const max = 20 
 
-console.log(Math.floor(Math.random() * (max - min +1 ))+min);
+console.log(Math.floor(Math.random() * (max - min + 1 ))+min);
+//line number 34 is important
