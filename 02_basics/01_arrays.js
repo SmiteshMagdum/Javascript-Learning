e79f36a3cc609ac89ac1305c3243346654bc50e8
SmiteshMagdum <smiteshmagdum@gmail.com>
2024-12-25 11:08:13 +0530

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+const myarr =[0,1,2,3,4,5]
+const myHeros = ["shaktiman","spiderman "]
+const  myArr2 = new Array (1,2,3,4)
+
+//console.log(myHeros[1])
+
+//Array methods 
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+
+
+//myarr.unshift(9)
+// myarr.shift()
+
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(3));
+
+// const newArr = myarr.join()
+// console.log(myarr);
+// console.log(typeof newArr);
+
+//slice , splice 
+
+console.log("A",myarr);
+const myn1 = myarr.slice(1,  3)
+
+console.log(myn1);
+console.log("B", myarr);
+
+const myn2 = myarr.splice(1, 3)
+console.log("c", myarr);
+console.log(myn2);
+
+
+//[0,1,2,3,4,5]
+
