799b63d4c5ed7d09f88f824878c8fcf6ec15096d
SmiteshMagdum <smiteshmagdum@gmail.com>
2024-12-26 10:30:11 +0530

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,48 @@
+// singleton 
+
+//object literals 
+//Object.create => singelton  
+
+const mySym = Symbol("Key1")
+
+
+
+const Jsuser = {
+    name: "smitesh",
+    "full name ": "smitesh magdum ",
+    [mySym]: "myKey1",
+    age : 18 ,
+    location:"mumbai",
+    email: "smitesh@gmail.com ",
+    isLoggedIn: false,
+    lastLoginDays: ["monday ", "staurday "]
+
+}
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name "]);
+console.log(Jsuser[mySym]);
+// -> smitesh@gmail.com
+// -> smitesh@gmail.com
+// -> smitesh  magdum 
+// -> myKey1 
+
+Jsuser.email = "smitesh@chatgpt.com "
+//Object.freeze(Jsuser)
+Jsuser.email = "smitesh@infosys.com"
+console.log(Jsuser);
+// -> after frezze value doesnot change 
+
+
+Jsuser.greeting = function (){
+    console.log("hello JS user ");
+}
+Jsuser.greetingTwo = function (){
+    console.log(`hello JS user, ${this.name}`);
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
+// -> hello JS user 
+// -> hello JS user , smitesh
+
