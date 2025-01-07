3da1599e6367d890302269f9eaa5e1c5c5bbb4b2
SmiteshMagdum <smiteshmagdum@gmail.com>
2025-01-07 19:55:25 +0530

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+
+// let index = 0 
+// while (index <= 10) {
+//     console.log(`value of index is ${index}` );
+//     index = index + 2
+// }
+
+// let myArr = ["flash", "batman","smitesh"]
+// let arr = 0 
+// while (arr < myArr.length) {
+//     console.log(`best hero is  ${myArr[arr]}`);
+//     arr = arr +1
+// }
+
+//*************************************************************************** */
+//do while looop 
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10 );
